5057e131f422f9d7550162c86a92bd948f5ab25c
Chad Hoang <chad.hoang@yahoo.com>
2016-10-08 16:11:08 -0500

    Parsed everything from file.

--- COSC 3360 - Assignment1/OS_HW1_Chad_Hoang.cpp
+++ COSC 3360 - Assignment1/OS_HW1_Chad_Hoang.cpp
@@ -23,20 +23,26 @@ struct Process
 {
 	int ID;
 	int deadline;
-	int computeTime;
+	int computeTime;					//	Integer equal to number of requests and releases plus the parenthesized values in the calculate and useresources instructions.
+	string *instructions;				//	Array of instructions for the processor
 };
-Process **processes;					//	Array that contains all the structures of the processes and the if it is read/write
+Process *processes;						//	Array that contains all the structures of the processes
 int **maxResourcePerProcess;
-
 
 //	Methods
 void ReadFromFile(string inputFileName);
 int GetFirstIntInString(string inputString);
 int GetMaxResourcePerProcessorValue(string inputString);
+void calculate(int computeTime);
+void request();
+void release();
+void useresources(int value);
+
+
 
 int main(int argc, char* argv[])
 {
-	//	Read, Evaluate, and Assign variables based in the input .txt file
+	//	Read, Evaluate, and Assign variables based in the input .txt file supplied by command argument
 	ReadFromFile(argv[1]);
 
 	//	Create the process. Created process is clone of this process
@@ -90,9 +96,9 @@ void ReadFromFile(string inputFileName)
 		numOfProcesses = GetFirstIntInString(currentLine);
 		
 		//	Initialize size of processes array and pipe read/write size which is 2
-		processes = new Process*[numOfProcesses];
-		for (int i = 0; i < numOfProcesses; i++)
-			processes[i] = new Process[2];
+		processes = new Process[numOfProcesses];
+		//for (int i = 0; i < numOfProcesses; i++)
+			//processes[i] = new Process[2];
 		cout << "Processes: " << numOfProcesses << "\n\n";
 
 		//	Determine the ID and amount of resources each resource has
@@ -111,14 +117,6 @@ void ReadFromFile(string inputFileName)
 
 		cout << endl;	//	Skip a line for neatness
 
-		//	Processes
-		for (int i = 0; i < numOfProcesses; i++)
-		{
-			//	Create new process struct and add it to array of resources
-		}
-
-		//cout << endl;	//	Skip a line for neatness
-
 		//	Find & Assign the size of demands for each resource per process
 		maxResourcePerProcess = new int*[numOfProcesses];
 		for (int i = 0; i < numOfProcesses * numOfResources; i++)
@@ -127,6 +125,8 @@ void ReadFromFile(string inputFileName)
 		//	Loop through the each process and assign the value of the max value processor can demand from each resource
 		for (int i = 0; i < numOfProcesses; i++)
 		{
+			cout << "Max resources Process " << i + 1 << " that can demand from:" << endl;
+			
 			for (int j = 0; j < numOfResources; j++)
 			{
 				//	Get new line and find value in string
@@ -134,11 +134,65 @@ void ReadFromFile(string inputFileName)
 				maxResourcePerProcess[i][j] = GetMaxResourcePerProcessorValue(currentLine);
 				
 				//	Display result
-				cout << "max[" << i + 1 << "," << j + 1 << "]" << ": " << maxResourcePerProcess[i][j] << endl;
+				cout << "Resource " << j + 1 << ": " << maxResourcePerProcess[i][j] << endl;
 			}
 		}
 
 		cout << endl;	//	Skip a line for neatness
+
+		//	Loop through each process and cache their parameters
+		for (int i = 0; i < numOfProcesses; i++)
+		{
+			//	Skip all lines until next process
+			while (true)
+			{
+				getline(inputFile, currentLine);
+				if (currentLine.find("process_") != string::npos)
+					break;
+			}
+			
+			cout << "Parameters for " << currentLine << endl << endl;
+
+			//	Deadline
+			getline(inputFile, currentLine);
+			processes[i].deadline = GetFirstIntInString(currentLine);
+			cout << "Process " << i+1 << " deadline: " << processes[i].deadline << endl;
+			
+			//	Compute time
+			getline(inputFile, currentLine);
+			processes[i].computeTime = GetFirstIntInString(currentLine);
+			cout << "Process " << i+1 << " compute time: " << processes[i].computeTime << endl << endl;
+
+			//	Calculate & Assign the amount of instructions for this process
+			int instructionAmount = 0;			
+			streampos originalPos = inputFile.tellg();		//	Cache line position
+			while (true)
+			{
+				getline(inputFile, currentLine);
+
+				//	Break loop if a "end" line is found & assign the length of instructions array
+				if (currentLine.find("end") != string::npos)
+				{
+					processes[i].instructions = new string[instructionAmount];
+					inputFile.seekg(originalPos, ios::beg);			//	Set the getline back to the original position
+					break;
+				}
+				instructionAmount++;
+			}
+
+			cout << "Instructions:" << endl;
+
+			//	Loop through instructions and cache them into process string array
+			for (int j = 0; j < instructionAmount; j++)
+			{
+				getline(inputFile, currentLine);
+				processes[i].instructions[j] = currentLine;
+
+				cout << " " << j+1 << ") " << processes[i].instructions[j] << endl;
+			}
+
+			cout << endl;	//	Skip a line for neatness
+		}
 	}
 	else
 	{
@@ -176,5 +230,33 @@ int GetMaxResourcePerProcessorValue(string inputString)
 	string intSubString = inputString.substr(pos + 1);
 
 	return stoi(intSubString);
+}
+#pragma endregion
+
+#pragma region calculate():  calculate without using resources. wait?
+void calculate(int computeTime)
+{
+	
+}
+#pragma endregion
+
+#pragma region request():  request vector, m integers
+void request()
+{
+
+}
+#pragma endregion
+
+#pragma region release():  release vector, m integers
+void release()
+{
+
+}
+#pragma endregion
+
+#pragma region useresources():  use allocated resources
+void useresources(int value)
+{
+
 }
 #pragma endregion
